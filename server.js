3af5759eb6b6a4daa33f5875ffb37aa691ab2c79
NishiGaba <nishi.livemedia@gmail.com>
2017-07-27 19:16:26 +0530

    Server File Modified

--- server.js
+++ server.js
@@ -28,3 +28,71 @@ transporter.sendMail(mailOptions, (error, info) => {
     }
     console.log('Message %s sent: %s', info.messageId, info.response);
 });
+
+
+// var mailContent = "<center><table class='body-wrap' style='text-align:center;width:86%;font-family:arial,sans-serif;border:12px solid rgba(126, 122, 122, 0.08);border-spacing:4px 20px;'>\
+            //                         <tr><img src='https://s3.ap-south-1.amazonaws.com/lccbucket/logo.png' style='width:36%;'></tr>\
+            //                         <tr>\
+            //                             <td>\
+            //                                 <center>\
+            //                                     <table bgcolor='#FFFFFF' width='80%' border='0'>\
+            //                                         <tbody>\
+            //                                             <tr>\
+            //                                                 <td style='font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:13px;color:#202020;line-height:1.5'>\
+            //                                                     <h1 style='color:#575252;text-align:center;'>Welcome to Livechek</h1>\
+            //                                                 </td>\
+            //                                             </tr>\
+            //                                             <tr style='text-align:center;color:#a2a2a2;font-size:14px;'>\
+            //                                                 <td>\
+            //                                                     <span>Your Agent has submitted your request to Company.</span>\
+            //                                                 </td>\
+            //                                             </tr>\
+            //                                             <tr style='text-align:center;color:#a2a2a2;font-size:14px;height:45px;'>\
+            //                                                 <td>\
+            //                                                     <span>You will soon receive a SMS to start Inspection of your Car to be Insured.</span>\
+            //                                                 </td>\
+            //                                             </tr>\
+            //                                         </tbody>\
+            //                                     </table>\
+            //                                 </center>\
+            //                             </td>\
+            //                         </tr>\
+            //                     </table></center>";
+
+
+            //             var smtpTransport = nodemailer.createTransport("SMTP", {
+            //                 service: "Gmail",
+            //                 auth: {
+            //                     XOAuth2: {
+            //                       user: "no_reply@livechek.com", // Your gmail address.
+            //                                                             // Not @developer.gserviceaccount.com
+            //                       clientId: "418419214912-cl08oopqas4p0a2selbv5vvaoqfkvr5s.apps.googleusercontent.com",
+            //                       clientSecret: "XcUl7BEKka3xGs3WeEQMkUxr",
+            //                       refreshToken: "1/OFp2lt3Ffs0AHpHwIuzOmaM3Yisk-aa3GE10moiDg88"
+            //                     }
+            //                 }
+            //             });
+
+            // //             //instead of output.pdf dynamica file name will come in future
+
+            //             var mailOptions = {
+            //                 from: "meLivemedia@noreply.com",
+            //                 // to: maillist,
+            //                 to: "gabanishi45@gmail.com",
+            //                 subject: "LiveChek",
+            //                 generateTextFromHTML: true,
+            //                 html: mailContent
+            //                     // attachments: [{ 'filename': 'output.pdf', 'contents': data }]
+            //             };
+
+            //             smtpTransport.sendMail(mailOptions, function(error, response) {
+            //                 if (error) {
+            //                     console.log(error);
+            //                 } else {
+            //                     // sendSMS(mobileNumber, link);
+            //                     res.send("success");
+            //                 }
+            //                 smtpTransport.close();
+            //             })
+            //         }
+            //     });
